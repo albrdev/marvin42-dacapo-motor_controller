3b58afc09f2f2b711467d027ced53d51813ee87b
albrdev <albrdev@gmail.com>
2019-12-02 13:35:54 +0100

    motor code

---
+++ inc/DCMotorAssembly.hpp
@@ -0,0 +1,68 @@
+#ifndef __DCMOTORASSEMBLY_HPP__
+#define __DCMOTORASSEMBLY_HPP__
+
+#include <stdint.h>
+#include <Adafruit_MotorShield.h>
+#include "maths.h"
+
+class DCMotorAssembly
+{
+private:
+    static Adafruit_MotorShield s_Shield;
+    static bool s_ShieldInitialized;
+
+    const static size_t k_DeviceCount = 4U;
+    Adafruit_DCMotor* m_Devices[k_DeviceCount];
+
+    const static size_t k_VectorLength = 3U;
+    const static size_t k_DirectionCount = k_VectorLength * k_VectorLength;
+    constexpr static float k_DirectionMap[k_VectorLength][k_VectorLength][k_DeviceCount] =
+    {
+        // Down row
+        {
+            { -1, 0, 0, -1 },   // Down-Left
+            { -1, -1, -1, -1 }, // Down-Middle
+            { 0, -1, -1, 0 }    // Down-Right
+        },
+
+        // Middle row
+        {
+            { -1, 1, 1, -1 },   // Middle-Left
+            { 0, 0, 0, 0 },     // Center (Stopped)
+            { 1, -1, -1, 1 }    // Middle-Right
+        },
+
+        // Up row
+        {
+            { 0, 1, 1, 0 },     // Up-Left
+            { 1, 1, 1, 1 },     // Up-Middle
+            { 1, 0, 0, 1 }      // Up-Right
+        }
+    };
+
+public:
+    void Run(const float directionX, const float directionY, const float power = 1.0f);
+
+    void SetSpeed(const float value);
+    void SetSpeed(const size_t port, const float value);
+
+    void SetLeftSpeed(const float value);
+    void SetRightSpeed(const float value);
+    void SetFrontSpeed(const float value);
+    void SetBackSpeed(const float value);
+
+    void SetXSpeed(const float left, const float right);
+    void SetYSpeed(const float up, const float down);
+
+    void Rotate(float value);
+
+    void Halt(void);
+    void Halt(const size_t port);
+
+    void Begin(void);
+
+    DCMotorAssembly(void);
+    virtual ~DCMotorAssembly(void);
+};
+
+#endif // __DCMOTORASSEMBLY_HPP__
---
+++ src/DCMotorAssembly.cpp
@@ -0,0 +1,127 @@
+#include "DCMotorAssembly.hpp"
+
+Adafruit_MotorShield DCMotorAssembly::s_Shield;
+bool DCMotorAssembly::s_ShieldInitialized = false;
+
+void DCMotorAssembly::Run(const float directionX, const float directionY, const float power)
+{
+    size_t x = (size_t)denormalize11(clamp11(directionX), 0, DCMotorAssembly::k_VectorLength - 1U);
+    size_t y = (size_t)denormalize11(clamp11(directionY), 0, DCMotorAssembly::k_VectorLength - 1U);
+
+    for(size_t i = 0U; i < DCMotorAssembly::k_DeviceCount; i++)
+    {
+        SetSpeed(i, DCMotorAssembly::k_DirectionMap[x][y][i] * clamp01(power));
+    }
+}
+
+void DCMotorAssembly::SetSpeed(const float value)
+{
+    uint8_t speed = denormalize01(fabs(value), 0, 255);
+    uint8_t direction = sgn(value) > 0 ? FORWARD : BACKWARD;
+    if(value == 0.0f)
+    {
+        Halt();
+    }
+
+    for(size_t i = 0U; i < DCMotorAssembly::k_DeviceCount; i++)
+    {
+        m_Devices[i]->setSpeed(speed);
+        m_Devices[i]->run(direction);
+    }
+}
+
+void DCMotorAssembly::SetSpeed(const size_t port, const float value)
+{
+    if(value == 0.0f)
+    {
+        Halt(port);
+    }
+
+    m_Devices[port]->setSpeed(denormalize01(fabsf(value), 0, 255));
+    m_Devices[port]->run(sgn(value) > 0 ? FORWARD : BACKWARD);
+    m_Devices[port]->run(RELEASE);
+}
+
+void DCMotorAssembly::SetLeftSpeed(const float value)
+{
+    SetSpeed(0, value);
+    SetSpeed(2, value);
+}
+
+void DCMotorAssembly::SetRightSpeed(const float value)
+{
+    SetSpeed(1, value);
+    SetSpeed(3, value);
+}
+
+void DCMotorAssembly::SetFrontSpeed(const float value)
+{
+    SetSpeed(0, value);
+    SetSpeed(1, value);
+}
+
+void DCMotorAssembly::SetBackSpeed(const float value)
+{
+    SetSpeed(2, value);
+    SetSpeed(3, value);
+}
+
+void DCMotorAssembly::SetXSpeed(const float left, const float right)
+{
+    SetLeftSpeed(left);
+    SetRightSpeed(right);
+}
+
+void DCMotorAssembly::SetYSpeed(const float up, const float down)
+{
+    SetFrontSpeed(up);
+    SetBackSpeed(down);
+}
+
+void DCMotorAssembly::Rotate(float value)
+{
+    if(value == 0.0f)
+    {
+        Halt();
+        return;
+    }
+
+    value = clamp11(value);
+    SetXSpeed(value, -value);
+}
+
+void DCMotorAssembly::Halt(void)
+{
+    for(size_t i = 0U; i < DCMotorAssembly::k_DeviceCount; i++)
+    {
+        Halt(i);
+    };
+}
+
+void DCMotorAssembly::Halt(const size_t port)
+{
+    m_Devices[port]->setSpeed(0);
+    //m_Devices[port]->run(RELEASE);
+}
+
+void DCMotorAssembly::Begin(void)
+{
+    if(!s_ShieldInitialized)
+    {
+        s_Shield.begin();
+        s_ShieldInitialized = true;
+    }
+
+    for(uint8_t i = 0U; i < DCMotorAssembly::k_DeviceCount; i++)
+    {
+        m_Devices[i] = s_Shield.getMotor(i + 1U);
+    }
+}
+
+DCMotorAssembly::DCMotorAssembly(void)
+{
+}
+
+DCMotorAssembly::~DCMotorAssembly(void)
+{
+}
