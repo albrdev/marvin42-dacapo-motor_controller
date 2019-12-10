cd24a338d4284d44e0ec8f5e109787415d7de7c3
Bassesan <sebastian.cobert@gmail.com>
2019-12-10 12:16:10 +0100

    added documentation

--- inc/DCMotorAssembly.hpp
+++ inc/DCMotorAssembly.hpp
@@ -7,37 +7,113 @@
 class DCMotorAssembly
 {
 private:
-    static Adafruit_MotorShield s_Shield;
-    static bool s_ShieldInitialized;
+    static Adafruit_MotorShield s_Shield; /*!< TODO */
+    static bool s_ShieldInitialized; /*!< TODO */
 
-    const static size_t k_DeviceCount = 4U;
-    Adafruit_DCMotor* m_Devices[k_DeviceCount];
+    const static size_t k_DeviceCount = 4U; /*!< TODO */
+    Adafruit_DCMotor* m_Devices[k_DeviceCount]; /*!< TODO */
 
-    const static size_t k_VectorLength = 3U;
-    const static size_t k_DirectionCount = k_VectorLength * k_VectorLength;
-    static const float k_DirectionMap[k_VectorLength][k_VectorLength][k_DeviceCount];
+    const static size_t k_VectorLength = 3U; /*!< TODO */
+    const static size_t k_DirectionCount = k_VectorLength * k_VectorLength; /*!< TODO */
+    static const float k_DirectionMap[k_VectorLength][k_VectorLength][k_DeviceCount]; /*!< TODO */
 
 public:
+    /*! Run().
+    \fn Run().
+    \param .
+    \return .
+    */
     void Run(const float directionX, const float directionY, float power = 1.0f);
 
+    /*! SetSpeed().
+    \fn SetSpeed().
+    \param .
+    \return .
+    */
     void SetSpeed(const float value);
+    
+    /*! SetSpeed().
+    \fn SetSpeed().
+    \param .
+    \return .
+    */
     void SetSpeed(const size_t port, const float value);
-
+    
+    /*! SetLeftSpeed().
+    \fn SetLeftSpeed().
+    \param .
+    \return .
+    */
     void SetLeftSpeed(const float value);
+    
+    /*! SetRightSpeed().
+    \fn SetRightSpeed().
+    \param .
+    \return .
+    */
     void SetRightSpeed(const float value);
+
+    /*! SetFrontSpeed().
+    \fn SetFrontSpeed().
+    \param .
+    \return .
+    */
     void SetFrontSpeed(const float value);
+
+    /*! SetBackSpeed().
+    \fn SetBackSpeed().
+    \param .
+    \return .
+    */
     void SetBackSpeed(const float value);
 
+    /*! SetXSpeed().
+    \fn SetXSpeed().
+    \param .
+    \return .
+    */
     void SetXSpeed(const float left, const float right);
+
+    /*! SetYSpeed().
+    \fn SetYSpeed().
+    \param .
+    \return .
+    */
     void SetYSpeed(const float up, const float down);
 
+    /*! Rotate().
+    \fn Rotate().
+    \param .
+    \return .
+    */
     void Rotate(float value);
 
+    /*! Halt().
+    \fn Halt().
+    \param .
+    \return .
+    */
     void Halt(void);
+
+    /*! Halt().
+    \fn Halt().
+    \param .
+    \return .
+    */
     void Halt(const size_t port);
 
+    /*! Begin().
+    \fn Begin().
+    \param .
+    \return .
+    */
     void Begin(void);
 
+    /*! DCMotorAssembly().
+    \fn DCMotorAssembly().
+    \param .
+    \return .
+    */
     DCMotorAssembly(void);
     virtual ~DCMotorAssembly(void);
 };
