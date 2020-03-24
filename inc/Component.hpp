caeda2c3379559b9bc5c726823b768e80906ea8b
albrdev <albrdev@gmail.com>
2020-03-24 01:32:41 +0100

    -

--- inc/Component.hpp
+++ inc/Component.hpp
@@ -3,7 +3,6 @@
 
 #include <stddef.h>
 #include <stdint.h>
-#include <Arduino.h>
 
 template <size_t N>
 class Component
@@ -12,33 +11,7 @@ protected:
     uint8_t m_Pins[N];
 
     template <typename ... I>
-    Component(const I& ... i) : m_Pins { i... } { }
-};
-
-template <size_t N>
-class Output
-{
-protected:
-    uint8_t m_Pins[N];
-
-    template <typename ... I>
-    Output(const I& ... i) : m_Pins{ i... }
-    {
-        pinMode(i..., OUTPUT);
-    }
-};
-
-template <size_t N>
-class Input
-{
-protected:
-    uint8_t m_Pins[N];
-
-    template <typename ... I>
-    Input(const I& ... i) : m_Pins{ i... }
-    {
-        pinMode(i..., INPUT);
-    }
+    Component(const I& ... i) : m_Pins{ i... } { }
 };
 
 #endif // __COMPONENT_HPP__
